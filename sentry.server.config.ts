d2ba8f2d2fb9d1a6dec2685ceaf2969a5cfea6ae
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2026-05-01 17:51:45 +0530

    setup the sentry setup for error tracking

---
+++ sentry.server.config.ts
@@ -0,0 +1,13 @@
+// Import with `import * as Sentry from "@sentry/nextjs"` if you are using ESM
+const Sentry = require("@sentry/nextjs");
+
+Sentry.init({
+  dsn: "https://e92e3d379658d3642a8248a308ffe5bc@o4511314497110016.ingest.de.sentry.io/4511314499469392",
+  // Tracing must be enabled for agent monitoring to work
+  tracesSampleRate: 1.0,
+  // Add data like inputs and responses to/from LLMs and tools;
+  // see https://docs.sentry.io/platforms/javascript/data-management/data-collected/ for more info
+  sendDefaultPii: true,
+  enableLogs: true,
+  debug: false
+});
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,16 +1,14 @@
-import { inngest } from '@/inngest/client';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
-import prisma from '@/lib/db';
-import { google } from '@ai-sdk/google';
-import { generateText } from 'ai';
+import { inngest } from "@/inngest/client";
+import prisma from "@/lib/db";
+import { createTRPCRouter, protectedProcedure } from "../init";
 
 export const appRouter = createTRPCRouter({
   testAi: protectedProcedure.mutation(async () => {
     await inngest.send({
-      name: 'execute/ai',
+      name: "execute/ai",
     });
 
-    return { success: true, message: 'Job Queued...' };
+    return { success: true, message: "Job Queued..." };
   }),
 
   getWorkflows: protectedProcedure.query(({ ctx }) => {
@@ -19,20 +17,18 @@ export const appRouter = createTRPCRouter({
 
   createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
-      name: 'app/task.created',
+      name: "app/task.created",
       data: {
-        email: 'tam@gmail.com',
+        email: "tam@gmail.com",
       },
     });
 
     return prisma.workflow.create({
       data: {
-        name: 'test-workflow',
+        name: "test-workflow",
       },
     });
   }),
-
-
 });
 
 // export type definition of API
