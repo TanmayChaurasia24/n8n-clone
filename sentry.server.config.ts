8bdea25e39fb038521c9e184788ecac8b2485c06
tanmay kumar chaurasia <tanmaychaurasia88@gmail.com>
2026-05-01 17:59:38 +0530

    improved coderabbit suggestions

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,8 +1,8 @@
 // Import with `import * as Sentry from "@sentry/nextjs"` if you are using ESM
-const Sentry = require("@sentry/nextjs");
+import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: "https://e92e3d379658d3642a8248a308ffe5bc@o4511314497110016.ingest.de.sentry.io/4511314499469392",
+  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
   // Tracing must be enabled for agent monitoring to work
   tracesSampleRate: 1.0,
   // Add data like inputs and responses to/from LLMs and tools;
